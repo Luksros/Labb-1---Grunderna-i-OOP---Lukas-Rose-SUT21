160fc524426a480d30b87ceb1e348d7ff1438869
Luksros <lukasrose@live.se>
2021-10-31 21:17:51 +0100

    Add project files.

---
+++ Grunderna i OOP/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grunderna_i_OOP
+{
+    class Circle
+    {
+        float pi = 3.141f;
+        int radius;
+
+        public Circle(int inputRadius)
+        {
+            radius = inputRadius;
+        }
+
+        public float GetArea()
+        {
+            return (radius * radius * pi);
+        }
+        public float GetCircumference()
+        {
+            return ((radius * 2) * pi);
+        }
+        public float GetVolume()
+        {
+            return ((4 * pi * (radius * radius * radius)) / 3);
+        }
+        public void CalculateAll()
+        {
+            Console.WriteLine("Arean på en cirkel med radien " + radius + " är " + GetArea());
+            Console.WriteLine("Omkretsen på en cirkel med radien " + radius + " är " + GetCircumference());
+            Console.WriteLine("Volymen på ett klot med radien " + radius + " är " + GetVolume());
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Grunderna i OOP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Grunderna_i_OOP
+    //LUKAS ROSE
+    //SUT21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle five = new Circle(5);
+            Circle six = new Circle(6);
+            five.CalculateAll();
+            six.CalculateAll();
+
+            Console.WriteLine("Skriv in en egen radie på en cirkel/ett klot: ");
+            int userRadius;
+            while (!int.TryParse(Console.ReadLine(), out userRadius))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Ogilting input. ");
+                Console.ResetColor();
+                Console.Write("Skriv in radien som ett heltal: ");
+            }
+
+            Circle userCircle = new Circle(userRadius);
+            userCircle.CalculateAll();
+
+            Console.ReadLine(); 
+        }
+    }
+}
